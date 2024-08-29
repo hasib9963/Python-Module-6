28bfdaa7412e25b208a9c56a976869cc046dda3e
Hasib Mohammad Mahede Hasan <hasib9963@gmail.com>
2024-08-30 05:39:04 +0600

    concept of inheritance

---
+++ inheritance_concept.py
@@ -0,0 +1,18 @@
+# base class/ parent class
+class BaseClass:
+    pass
+
+# derived class or child class
+class DerivedClass:
+    pass
+
+
+""" 
+1. Simple inheritance: parent class --> child class (gadget --> Phone) (gadget --> Laptop)
+
+2. Multi-level inheritance : Grandpa --> parent --> child (Vehicle --> Bus --> AC bus)
+
+3. Multiple inheritance: Student (Family, School, Sports)
+
+4. Hybrid inheritance : Grandpa --> Father, Uncle, Aunty --> Child (Father, Uncle)
+"""
